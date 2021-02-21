0b4108b4ccc7047b2f1a55dbc8c80123837c1ccc
elguneminov <68548878+elguneminov@users.noreply.github.com>
2021-02-21 14:03:40 +0400

    Add files via upload

---
+++ spiral.py
@@ -0,0 +1,12 @@
+num = int(input("Qizinin herf sayi:"))
+c = int(num/2)
+
+n = 3+c
+
+for i in range(n):
+    for j in range(num):
+        if (i==0 and j%c!=0) or (i==1 and j%c==0) or i-j==2 or i+j==num+1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
